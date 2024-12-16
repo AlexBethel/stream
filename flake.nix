a02286df901e38ec7d3b91d2ee5fe5140e885c80
Alex Bethel <alex.b.bethel@gmail.com>
2024-12-15 22:19:02 -0700

    fix typo

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         streamScript = streamName: pkgs.writeScriptBin streamName ''
           #!${pkgs.bash}/bin/bash
-          export PATH=${pkgs.ffmpeg}/bin:${pkgs.yt-dlp}:$PATH
+          export PATH=${pkgs.ffmpeg}/bin:${pkgs.yt-dlp}/bin:$PATH
           ${./main.sh} ${streamName}
         '';
       in
